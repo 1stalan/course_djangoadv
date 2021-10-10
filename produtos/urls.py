a6752cbc8d22dc76ca7aaedd7617660e127a7337
Alan Santos <alan.profissional@outlook.com>
2021-10-10 19:25:24 -0300

    Templatede produtos

--- produtos/urls.py
+++ produtos/urls.py
@@ -1,5 +1,14 @@
 from django.urls import path
 from .views import ProdutoBulk
+from .views import ProdutoHome
+from .views import ProdutoCreate
+from .views import ProdutoUpdate
+from .views import ProdutoDelete
 urlpatterns = [
+    path('list/', ProdutoHome.as_view(), name="produtos"),
+    path('produto_create/', ProdutoCreate.as_view(), name="produto_create"),
+    path('produto_update/<int:pk>/', ProdutoUpdate.as_view(), name="produto_update"),
+    path('produto_delete/<int:pk>/', ProdutoDelete.as_view(), name="produto_delete"),
     path('produto_bulk/', ProdutoBulk.as_view(), name="produto_bulk"),
+
 ]
--- produtos/views.py
+++ produtos/views.py
@@ -1,7 +1,43 @@
+from time import timezone
 from django.http import HttpResponse
+from django.urls import reverse_lazy
 from django.views import View
+from django.views.generic import ListView
+from django.views.generic import DetailView
+from django.views.generic import CreateView
+from django.views.generic import UpdateView
+from django.views.generic import DeleteView
+
+
 from .models import Produto
 
+class ProdutoHome(ListView):
+    model = Produto
+    template_name = 'produtos/produtos_home.html'
+
+class ProdutoDetail(DetailView):
+    model = Produto
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['now'] = timezone.now()
+        return context
+
+class ProdutoCreate(CreateView):
+    model = Produto
+    fields = ['descricao','preco']
+    success_url = reverse_lazy('produtos')
+
+class ProdutoUpdate(UpdateView):
+    model = Produto
+    fields = ['descricao', 'preco']
+    success_url = reverse_lazy('produtos')
+
+
+class ProdutoDelete(DeleteView):
+    model = Produto
+
+    success_url = reverse_lazy('produtos')
 
 class ProdutoBulk(View):
     def get(self, request):
